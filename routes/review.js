a2cf73ce8a655771969073e05688b6170f3cb0ec
My Name <gunanivivek@gmail.com>
2025-01-08 02:46:44 +0530

    Add initial files

---
+++ routes/review.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const {validateReview,isLoggedIn,isReviewAuthor} = require("../middleware.js");
+const reviewController = require("../controllers/review.js");
+
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
+
+//DELETE REVIEW
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  wrapAsync(reviewController.destroyReview)
+);
+
+module.exports = router;
